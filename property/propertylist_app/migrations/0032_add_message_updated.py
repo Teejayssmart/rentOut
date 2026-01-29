5aa29213400e39c5480c6ce189380527971040be
Teejayssmart <teejaysmarty@gmail.com>
2026-01-29 22:50:56 +0000

    Neutralise broken message updated migration

--- property/propertylist_app/migrations/0032_add_message_updated.py
+++ property/propertylist_app/migrations/0032_add_message_updated.py
@@ -6,14 +6,4 @@ class Migration(migrations.Migration):
         ("propertylist_app", "0031_remove_room_uq_room_title_lower_alive_and_more"),
     ]
 
-    operations = [
-        migrations.RunSQL(
-            sql="""
-            CREATE INDEX IF NOT EXISTS message_thread_updated_idx
-            ON propertylist_app_message (thread_id, updated);
-            """,
-            reverse_sql="""
-            DROP INDEX IF EXISTS message_thread_updated_idx;
-            """,
-        ),
-    ]
+    operations = []
