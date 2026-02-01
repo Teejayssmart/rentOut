040053f92cdea14d034b42533abb3abd86aa88b1
Teejayssmart <teejaysmarty@gmail.com>
2026-02-01 01:40:43 +0000

    Disable DRF authtoken creation signal (using SimpleJWT)

--- property/user_app/models.py
+++ property/user_app/models.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+# NOTE:
+# This project uses JWT (SimpleJWT). We do not create DRF authtoken Token rows.
+# Creating tokens here breaks deploy because Token auth is not part of this setup.
+
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_auth_token(sender, instance=None, created=False, **kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
