cb97343bddccba3230f28352eb26161e58512794
Teejayssmart <teejaysmarty@gmail.com>
2026-02-04 01:20:27 +0000

    Ensure Swagger schema routes exist

--- property/property/urls.py
+++ property/property/urls.py
@@ -28,16 +28,8 @@ urlpatterns = [
     path("api/auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path(
-        "api/schema/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
-    ),
+    path("api/schema/swagger-ui/",SpectacularSwaggerView.as_view(url_name="schema"),name="swagger-ui",),
+    path("api/schema/redoc/",SpectacularRedocView.as_view(url_name="schema"),name="redoc",),
 ]
 
 if settings.DEBUG:
