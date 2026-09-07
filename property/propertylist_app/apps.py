04e44b38bf333c3be01df139df7f8de1959a518a
Teejay Adedosu <teejaysmarty@gmail.com>
2026-09-07 13:02:18 +0100

    Install booking list query optimization

--- property/propertylist_app/apps.py
+++ property/propertylist_app/apps.py
@@ -7,3 +7,8 @@ class PropertylistAppConfig(AppConfig):
     def ready(self):
         # import signals so receivers are registered
         from . import signals  # noqa: F401
+
+        from propertylist_app.services.booking_list_query_optimization import (
+            install_booking_list_query_optimization,
+        )
+        install_booking_list_query_optimization()
