217cbbf9197696977438eff5600c2f79903a7e2d
Teejay Adedosu <teejaysmarty@gmail.com>
2026-09-08 09:00:12 +0100

    Optimize my rooms related queries

--- property/propertylist_app/apps.py
+++ property/propertylist_app/apps.py
@@ -20,9 +20,13 @@ class PropertylistAppConfig(AppConfig):
         from .services.booking_list_query_optimization import (
             install_booking_list_query_optimization,
         )
+        from .services.my_rooms_query_optimization import (
+            install_my_rooms_related_query_optimization,
+        )
 
         install_room_serializer_image_cache()
         install_homepage_owner_profile_query_optimization()
         install_saved_rooms_related_query_optimization()
         install_availability_only_free_query_optimization()
         install_booking_list_query_optimization()
+        install_my_rooms_related_query_optimization()
